c9ab219dac4795be76e6017a7ceffa9d6f55d924
Amish <amish.tufail2002@gmail.com>
2022-03-21 14:13:36 +0500

    - Modify the card further

--- Learning App/ContentView.swift
+++ Learning App/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
+            Spacer() // pushed everything to bottom from image to mutlitext
             Image("Logo 2")
                 .resizable(resizingMode: .stretch) // from inspector
                 .aspectRatio(contentMode: .fit)
@@ -21,16 +22,21 @@ struct ContentView: View {
             Text("20 sections - 3 hours")
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build and iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading) // from inspector
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading) // infinity = max possible space occupied
+                .foregroundColor(.secondary)
         }
         .padding(.all, 20.0)
+        .padding(.vertical, 20.0) // padding inside the card top and bottom
         .frame(height: 350.0)
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+        .background(Color("Background"))
         .cornerRadius(30.0)
-        .shadow(radius: 20.0)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10.0, x: 0.0, y: 10.0)
+        .padding(.horizontal, 20.0) // padding on the outer horizontal
     }
 }
 

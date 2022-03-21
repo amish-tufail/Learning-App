54c1cd32c0cc60a3d3c2ea6ef6949b10ecb4186c
Amish <amish.tufail2002@gmail.com>
2022-03-21 15:24:32 +0500

    - Add A Material Sheet to background of card - Add another background with a blob image - Add an overlay on the card with a image - Add Gradient Color to main Text - Add ForegroundStyle

--- Learning App/ContentView.swift
+++ Learning App/ContentView.swift
@@ -19,10 +19,11 @@ struct ContentView: View {
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            Text("20 sections - 3 hours")
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
             Text("Build and iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading) // from inspector
@@ -33,15 +34,29 @@ struct ContentView: View {
         .padding(.all, 20.0)
         .padding(.vertical, 20.0) // padding inside the card top and bottom
         .frame(height: 350.0)
-        .background(Color("Background"))
+        .background(.ultraThinMaterial) // using instead of Color("Background")
         .cornerRadius(30.0)
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10.0, x: 0.0, y: 10.0)
         .padding(.horizontal, 20.0) // padding on the outer horizontal
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+            
+                
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }

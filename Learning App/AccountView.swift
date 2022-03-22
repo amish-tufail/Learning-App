dd6d3e8e2e3152f0e487e1c4a5438f062e011330
Amish <amish.tufail2002@gmail.com>
2022-03-22 17:42:09 +0500

    - Update the AccountView with Links and NavigationLinks - Add more details to the user section

--- Learning App/AccountView.swift
+++ Learning App/AccountView.swift
@@ -9,16 +9,72 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        List {
-            Section {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                Text("Billing")
-                Text("Help")
+        NavigationView {
+            List {
+                VStack(spacing: 8.0) {
+                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .symbolRenderingMode(.palette)
+                        .font(.system(size: 32))
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                    )
+                    Text("Amish")
+                        .font(.title.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.medium)
+                        Text("Pakistan")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                Section {
+                    NavigationLink { ContentView() } label: {
+                        Label("Settings", systemImage: "gear")
+                    }
+                    NavigationLink { Text("Billing") } label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    NavigationLink { Text("Help") } label: {
+                        Label("Help", systemImage: "questionmark")
+                    }
+                }
+                .accentColor(.primary)
+                .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+                Section {
+                    Link(destination: URL(string: "https://apple.com")!) {
+                        HStack {
+                            Label("Website", systemImage: "house")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    Link(destination: URL(string: "https://youtube.com")!) {
+                        HStack {
+                            Label("Youtube", systemImage: "tv")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .listRowSeparator(.hidden)
+                .accentColor(.primary)
             }
-            .listRowSeparatorTint(.blue)
-            .listRowSeparator(.hidden)
+            .listStyle(.insetGrouped) // would adopt automatic to different devices
+            .navigationTitle("Account")
         }
-        .listStyle(.automatic) // would adopt automatic to different devices
     }
 }
 

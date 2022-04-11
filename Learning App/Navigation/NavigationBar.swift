d2b3b8aa44cc1b66e001328c7a011fde924e5379
Amish <amish.tufail2002@gmail.com>
2022-04-11 23:26:24 +0500

    - Add buttons to the Navigation Bar

--- Learning App/Navigation/NavigationBar.swift
+++ Learning App/Navigation/NavigationBar.swift
@@ -17,10 +17,27 @@ struct NavigationBar: View {
             Text(title)
                 .font(.largeTitle.weight(.bold))
                 .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.leading, 20)
+                .padding(.leading, 20)
+            HStack(spacing: 16) {
+                Image(systemName: "magnifyingglass")
+                    .font(.body.weight(.bold))
+                    .frame(width: 36, height: 36)
+                    .foregroundColor(.secondary)
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 14, style: .continuous))
+                    .strokeStyle(cornerRadius: 14)
+                Image("Avatar Default")
+                    .resizable()
+                    .frame(width: 26, height: 26)
+                    .cornerRadius(10)
+                    .padding(8)
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+                    .strokeStyle(cornerRadius: 18)
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.trailing, 20)
         }
-            .frame(height: 70) // height of background
-            .frame(maxHeight: .infinity, alignment: .top)
+        .frame(height: 70) // height of background
+        .frame(maxHeight: .infinity, alignment: .top)
     }
 }
 
